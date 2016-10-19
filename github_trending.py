8a05ff70e9b64e13869083757c531e15983afae0
Shcherbakov Alexey <lovelessxt@gmail.com>
2016-10-19 19:47:08 +0300

    Добавлен код для работы с Git API
    Добавлена функция string_date_num_days_ago(), которая возвращает дату, что была num_days дней назад в формате год-месяц-день.
    
    Добавлена функция get_trending_repositories(), которая взаимодействует с Git API и возвращает 20 наиболее популярных за последние 7 дней репозиториев
    
    Добавлена функция print_trending_repositories(), которая выводит в консоль наиболее популярные репозитории, ссылки на них, количество звезд и количество открытых задач.

--- github_trending.py
+++ github_trending.py
@@ -1,11 +1,36 @@
+# -*- coding: utf-8 -*-
+import requests
+import datetime
+
+GIT_API_URL = 'https://api.github.com/search/repositories'
+TOP_REP_SIZE = 20
+NUMBER_OF_DAYS = 7
 
 
-def get_trending_repositories(top_size):
-    pass
+def string_date_num_days_ago(num_days):
+    delta = datetime.timedelta(days=num_days)
+    date = datetime.datetime.now() - delta
+    return date.strftime('%Y-%m-%d')
 
 
-def get_open_issues_amount(repo_owner, repo_name):
-    pass
+def get_trending_repositories(top_size, api_url, num_days):
+    request_params = {
+        'q': '{}{}'.format('created:>=', string_date_num_days_ago(num_days)),
+        'sort': 'stars',
+        'order': 'desc'
+    }
+    trending_repositories_data = requests.get(api_url, params=request_params).json()
+    return trending_repositories_data['items'][:top_size]
+
+
+def print_trending_repositories(trending_repositories_data):
+    print(u'Наиболее популярные репозитории за последние {} дней:'.format(NUMBER_OF_DAYS))
+    for repository in trending_repositories_data:
+        print(u'Репозиторий {}:\nСсылка на репозиторий:{}; Количество звезд:{}; Количество открытых задач:{}'.format(
+            repository['name'], repository['html_url'], repository['stargazers_count'],
+            repository['open_issues_count']))
+
 
 if __name__ == '__main__':
-    pass
+    trending_repositories_data = get_trending_repositories(TOP_REP_SIZE, GIT_API_URL, NUMBER_OF_DAYS)
+    print_trending_repositories(trending_repositories_data)
